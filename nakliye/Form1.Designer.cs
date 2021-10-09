16dc9fdae7d71109ba25dbe4973dd974b716ca95
Altuğ Karakayalı <altugkarakayali@gmail.com>
2021-10-09 15:35:42 +0300

    Initial commit

---
+++ nakliye/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace nakliye
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 463);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(102, 27);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1 Duraklı";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Tahoma", 14.25F);
+            this.radioButton2.Location = new System.Drawing.Point(152, 463);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 27);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2 Duraklı";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Tahoma", 14.25F);
+            this.radioButton3.Location = new System.Drawing.Point(314, 463);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(102, 27);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3 Duraklı";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Tahoma", 14.25F);
+            this.radioButton4.Location = new System.Drawing.Point(470, 463);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(102, 27);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4 Duraklı";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Tahoma", 14.25F);
+            this.radioButton5.Location = new System.Drawing.Point(630, 463);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(102, 27);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5 Duraklı";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F);
+            this.button1.Location = new System.Drawing.Point(12, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(725, 73);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LightGray;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(720, 420);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(590, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(743, 583);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ nakliye/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nakliye
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[,] dizi = { { 0,290,845,919,1220,1140,1400,1540,1600,1689,1400,1521,1807},
+                        { 290,0,570,675,970,898,1144,1280,1298,1446,1156,1287,1500},
+                        { 845,570,0,450,660,710,8667,801,665,938,647,805,1020},
+                        { 919,675,450,0,305,270,521,612,633,855,643,808,968},
+                        { 1220,970,660,305,0,261,413,415,436,645,661,838,763},
+                        { 1140,898,710,270,261,0,629,657,678,887,806,963,1100},
+                        { 1400,1144,8667,521,413,629,0,232,314,344,229,366,430},
+                        { 1540,1280,801,612,415,657,232,0,122,236,389,523,420},
+                        { 1600,1298,665,633,436,678,314,122,0,311,472,615,480},
+                        { 1689,1446,938,855,645,887,344,236,311,0,302,367,200},
+                        { 1400,1156,647,643,661,806,229,389,472,302,0,163,390},
+                        { 1521,1287,805,808,838,963,366,523,615,367,163,0,323},
+                        { 1807,1500,1020,968,763,1100,430,420,480,200,390,323,0} };
+
+        string[] duraklar = {"Iğdır","Erzurum", "Samsun", "Kayseri", "Konya", "Mersin", "Eskişehir", "Uşak", "Denizli", "Balıkesir", "Izmit", "Istanbul","Canakkale" };
+        
+private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                listBox1.Items.Clear();
+                label1.Text = "Lütfen Bekleyiniz...";
+                this.Refresh();
+                for (int a = 1; a < 12; a++)
+                {
+                    listBox1.Items.Add(dizi[0, a] + dizi[a, 12] + " Km      Iğdır->" + duraklar[a] + "-> Çanakkale");
+                }
+                label1.Text = listBox1.Items.Count.ToString() + " olasılık bulundu.";
+
+            }
+            if (radioButton2.Checked)
+            {
+                listBox1.Items.Clear();
+                label1.Text = "Lütfen Bekleyiniz...";
+                this.Refresh();
+                for (int a = 1; a < 12; a++)
+                {
+                    for (int b = 1; b < 10; b++)
+                    {
+                        if (a==b)
+                        {
+                            continue;
+                        }
+                        listBox1.Items.Add(dizi[0, a] + dizi[a, b]+dizi[b,12]+ " Km      Iğdır->" + duraklar[a]+"->"+duraklar[b]+ "-> Çanakkale");
+                    }
+                }
+                label1.Text = listBox1.Items.Count.ToString() + " olasılık bulundu.";
+
+            }
+            if (radioButton3.Checked)
+            {
+                listBox1.Items.Clear();
+                label1.Text = "Lütfen Bekleyiniz...";
+                this.Refresh();
+                for (int a = 1; a < 12; a++)
+                {
+                    for (int b = 1; b < 12; b++)
+                    {
+                        for (int c = 1; c < 12; c++)
+                        {
+                            if (a == b || b == c)
+                            {
+                                continue;
+                            }
+                            listBox1.Items.Add(dizi[0, a] + dizi[a, b] + dizi[b, c]+dizi[c,12]+ " Km      Iğdır->" + duraklar[a] + "->" + duraklar[b] +"->"+duraklar[c]+ "-> Çanakkale");
+                           
+                        }
+                    }
+                }
+                label1.Text = listBox1.Items.Count.ToString() + " olasılık bulundu.";
+
+            }
+            if (radioButton4.Checked)
+            {
+                listBox1.Items.Clear();
+                label1.Text = "Lütfen Bekleyiniz...";
+                this.Refresh();
+                for (int a = 1; a < 12; a++)
+                {
+                    for (int b = 1; b < 12; b++)
+                    {
+                        for (int c = 1; c < 12; c++)
+                        {
+                            for (int d = 1; d < 12; d++)
+                            {
+                                if (a == b || b == c ||c==d)
+                                {
+                                    continue;
+                                }
+                              
+                                listBox1.Items.Add(dizi[0, a] + dizi[a, b] + dizi[b, c] +dizi[c,d]+ dizi[d, 12] +" Km      Iğdır->" + duraklar[a] + "->" + duraklar[b] + "->" + duraklar[c]+ "->" + duraklar[d] + "-> Çanakkale");
+                            }
+
+                        }
+                    }
+                }
+                label1.Text = listBox1.Items.Count.ToString() + " olasılık bulundu.";
+
+            }
+
+            if (radioButton5.Checked)
+            {
+                listBox1.Items.Clear();
+                label1.Text = "Lütfen Bekleyiniz...";
+                this.Refresh();
+                for (int a = 1; a < 12; a++)
+                {
+                    for (int b = 1; b < 12; b++)
+                    {
+                        for (int c = 1; c < 12; c++)
+                        {
+                            for (int d = 1; d < 12; d++)
+                            {
+                                for (int f = 0; f < 12; f++)
+                                {
+                                    if (a == b || b == c || c == d ||d==f)
+                                    {
+                                        continue;
+                                    }
+                                    
+                                    listBox1.Items.Add(dizi[0, a] + dizi[a, b] + dizi[b, c] + dizi[c, d] + dizi[d, f]+dizi[f,12] + " Km      Iğdır->" + duraklar[a] + "->" + duraklar[b] + "->" + duraklar[c] + "->" + duraklar[d] +"->"+duraklar[f]+ "-> Çanakkale");
+
+                                }
+                            }
+
+                        }
+                    }
+                }
+                label1.Text = listBox1.Items.Count.ToString()+" olasılık bulundu.";
+            }
+
+        }
+    }
+}
